4ddf0391b41e753e15fe70e3cb23db04a0deab88
Sumithanvat <skhanvat@gmail.com>
2023-06-03 00:58:09 +0530

    Added:Show table

--- src/main/java/com/jdbc_AddressBook/AddressBookSystem.java
+++ src/main/java/com/jdbc_AddressBook/AddressBookSystem.java
@@ -1,9 +1,6 @@
 package com.jdbc_AddressBook;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class AddressBookSystem {
     public static void main(String[] args) {
@@ -14,17 +11,36 @@ public class AddressBookSystem {
         String password = "Sumit@123";
 
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            // create table
 
-            try (Statement statement = connection.createStatement()) {
-                String createTableQuery = "create table address_book2 (id INT primary key,first_name varchar(20),last_name varchar(20),address varchar(50),city Varchar(20),state varchar(20),zip int,phone varchar(20),email varchar(20))";
-                statement.executeUpdate(createTableQuery);
-                System.out.println("Table is created");
-            }
-            try (Statement statement = connection.createStatement()) {
-                String insertQuery = "insert into address_book2 (id,first_name,last_name,address,city,state,zip,phone,email) values (1,'Sumit','Hanvat','Jabalpur','LKD','MP','480448','95858858','AVC@gmaiol.com')";
-                statement.executeUpdate(insertQuery);
-                System.out.println("value inserted successfully");
+            // Update query
+            try (Statement statement = connection.createStatement()){
+                String selectQuery = "select * from address_book2";
+                ResultSet resultSet = statement.executeQuery(selectQuery);
+
+                // Iterate over result set
+                while (resultSet.next()){
+                    int id = resultSet.getInt(1);
+                    String first_name = resultSet.getString(2);
+                    String last_name = resultSet.getString(3);
+                    String address = resultSet.getString(4);
+                    String city = resultSet.getString(5);
+                    String state = resultSet.getString(6);
+                    int zip = resultSet.getInt(7);
+                    String phone = resultSet.getString(8);
+                    String email = resultSet.getString(9);
+
+                    // Print the retrieved values
+                    System.out.println("ID: " + id);
+                    System.out.println("First Name: " + first_name);
+                    System.out.println("Last Name: " + last_name);
+                    System.out.println("Address: " + address);
+                    System.out.println("City: " + city);
+                    System.out.println("State: " + state);
+                    System.out.println("ZIP: " + zip);
+                    System.out.println("Phone: " + phone);
+                    System.out.println("Email: " + email);
+                }
+
             }
         } catch (SQLException e) {
             e.printStackTrace();
